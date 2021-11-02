a143f8ca83747d59543cea3a9d7d415d3600eb9e
moumou <mouad.melhaoui@helb-prigogine.be>
2021-11-02 22:40:38 +0100

    change adress api http

--- src/main/java/org/helb/baseproject/model/user/UserController.java
+++ src/main/java/org/helb/baseproject/model/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/user/0.1")
+@RequestMapping("api/user/")
 public class UserController {
 
     private final UserService userService;
